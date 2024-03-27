9f9910b0f7ad135f8e5ea656b570593d49ba46be
cdortch <CelenaDortch@gmail.com>
2024-03-27 08:18:16 -0500

    Initial Push

--- Magic8Ball/ViewController.swift
+++ Magic8Ball/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var magic8BallView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func shakeButtonPressed(_ sender: UIButton) {
+        let magic8BallTextArray = [UIImage(imageLiteralResourceName: "8BallM1"), UIImage(imageLiteralResourceName: "8BallM2"), UIImage(imageLiteralResourceName: "8BallM3"), UIImage(imageLiteralResourceName: "8BallM4"), UIImage(imageLiteralResourceName: "8BallM5"), UIImage(imageLiteralResourceName: "8BallM6")
+        ]
+        
+        magic8BallView.image = magic8BallTextArray[Int.random(in: 0...5)]
+    }
+    
 }
 
